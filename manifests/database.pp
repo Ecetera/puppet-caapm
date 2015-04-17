3b6853e38bfa93596207e0c4bca6947b5dde04a6
dimarra <raul.dimar@gmail.com>
2015-04-17 17:52:52 +1000

    database is now a defined type

--- manifests/database.pp
+++ manifests/database.pp
@@ -1,30 +1,44 @@
-# 
+#
 # == Class: caapm::database
 #
-# This class manages the CA APM Database PostgreSQL Bundle 
+# This class manages the CA APM Database PostgreSQL Bundle
 #
 #
-class caapm::database (
-  $version = $caapm::params::version,
-  $user_install_dir = $caapm::params::user_install_dir,  
-      
-  # APM Database Settings
-  $database = $caapm::params::apm_db,
-  $db_host = $caapm::params::db_host,
-  $db_port = $caapm::params::db_port,
-  $db_name = $caapm::params::db_name,
-  $db_user_name = $caapm::params::db_user_name,
-  $db_user_passwd = $caapm::params::db_user_passwd,
 
-  $postgres_dir = $caapm::params::pg_dir,
-  $pg_admin_user = $caapm::params::pg_admin_user,
-  $pg_admin_passwd = $caapm::params::pg_admin_passwd,
-  $pg_install_timeout = $caapm::params::pg_install_timeout,
-  $pg_as_service = $caapm::params::pg_as_service,
-   
-) inherits caapm::params {
-  
-  
+define caapm::database (
+  $version = $caapm::params::version,
+  $user_install_dir = $caapm::params::user_install_dir,
+
+  # APM Database Settings
+  $database = "postgres",
+  $db_host = "localhost",
+  $db_port = 5432,
+  $db_name = "cemdb",
+  $db_user_name = "admin",
+  $db_user_passwd = "wily",
+
+  $postgres_dir = "database",
+  $pg_admin_user = "postgres",
+  $pg_admin_passwd = "C@wilyapm90",
+  $pg_install_timeout = 240000,
+  $pg_as_service = true,
+
+  $owner  = undef,
+  $group  = undef,
+  $mode   = 755,
+
+){
+
+  $staging_subdir = "${module_name}"
+
+/*
+  notify {"database is $database":}
+  notify {"db_host is $db_host":}
+  notify {"caapm::params::apm_db is $caapm::params::apm_db":}
+  notify {"caapm::params::db_host is $caapm::params::db_host":}
+  notify {"caapm::params::db_name is $caapm::params::db_name":}
+ */
+
   $service_name = $version ? {
     '9.1.4.0' => $::operatingsystem ? {
         'windows' => 'pgsql-8.4',
@@ -44,23 +58,23 @@ class caapm::database (
       },
      default => undef
   }
-  
+
   class { "caapm::osgi":
     apmversion => $version,
   }
-  
-  
-  $staging_path = $staging::params::path 
-  
+
+
+  $staging_path = $staging::params::path
+
   $user_install_dir_em = $::operatingsystem ? {
     'windows' => to_windows_escaped("${user_install_dir}"),
     default  => "${user_install_dir}",
-  } 
-  
-/* ===================================================================== */ 
+  }
 
-  $pkg_name = "CA APM Introscope ${version}" 
-  
+/* ===================================================================== */
+
+  $pkg_name = "CA APM Introscope ${version}"
+
   $eula_file = 'ca-eula.txt'
   $resp_file = 'Database.ResponseFile.txt'
   $lic_file = "${ipaddress}.em.lic"
@@ -68,31 +82,31 @@ class caapm::database (
   $features = 'Database'
   $upgradeEM = false
   $clusterEM = false
-    
+
   $puppet_src = "puppet:///modules/${module_name}"
 
   $resp_src = "${puppet_src}/${resp_file}"
 
-  # determine the executable package  
+  # determine the executable package
   $pkg_bin = $::operatingsystem ? {
     'windows' => "introscope${version}${operatingsystem}AMD64.exe",
     default  => "introscope${version}linuxAMD64.bin",
   }
-  
-  # download the eula.txt  
+
+  # download the eula.txt
   staging::file { $eula_file:
     source => "${puppet_src}/${version}/${eula_file}",
     subdir => $staging_subdir,
-  }  
-  
-  # download the Enterprise Manager installer  
+  }
+
+  # download the Enterprise Manager installer
   staging::file { $pkg_bin:
     source => "${puppet_src}/${version}/${pkg_bin}",
     subdir => $staging_subdir,
     require => Staging::File[$eula_file],
-    
+
   }
-  
+
   # generate the response file
   file { $resp_file:
     path    => "$staging_path/$staging_subdir/$resp_file",
@@ -102,15 +116,15 @@ class caapm::database (
 #    source_permissions => ignore,
     owner   =>  $owner,
     group   =>  $group,
-    mode    =>  $mode,    
+    mode    =>  $mode,
   }
-  
+
   $install_options = $::operatingsystem ? {
     'windows' => "$staging_path\\$staging_subdir\\$resp_file",
     default   => "$staging_path/$staging_subdir/$resp_file",
   }
-  
-  
+
+
   file { $failed_log :
     path   => "$staging_path/$staging_subdir/$failed_log",
     ensure => absent,
@@ -128,7 +142,7 @@ class caapm::database (
         timeout     => 0,
         notify      => Service[$service_name],
       }
-      
+
     }
 
     windows: {
@@ -143,42 +157,12 @@ class caapm::database (
       }
     }
   }
-  
-  
+
+
         # ensure the service is running
         service { $service_name:
           ensure  => $pg_as_service,
           enable  => $pg_as_service,
         }
 
-  
-/* ======================================================================  
-
-  class {'caapm::em': 
-    version => $version,
-    features => 'Database',
-    user_install_dir => $user_install_dir,
-    
-    # APM Database Settings
-    database => $database,
-    db_host => $db_host,
-    db_port => $db_port,
-    db_name => $db_name,
-    db_user_name => $db_user_name,
-    db_user_passwd => $db_user_passwd,
-    postgres_dir => $postgres_dir,
-    pg_admin_user => $pg_admin_user,
-    pg_admin_passwd => $pg_admin_passwd,
-    pg_install_timeout => $pg_install_timeout,
-    config_as_service => false,
-    config_wv_as_service => false,
-    notify  => Service[$service_name],
-  }
- 
-    # ensure the service is running
-  service { $service_name:
-    ensure  => "running",
-    enable  => true,
-  }
- */
 }
--- manifests/profile/database.pp
+++ manifests/profile/database.pp
@@ -1,20 +1,28 @@
-# 
+#
 # == Class: caapm::profile::database
 #
-# This profile configures the defaults for the PostgreSQL APM DB 
+# This profile configures the defaults for the PostgreSQL APM DB
 #
-class caapm::profile::database { 
+
+#
+# TODO: make this pass on puppet-lint
+#
+
+class caapm::profile::database {
 
 #  $version = '9.1.4.0'
 #  $version = '9.6.0.0'
-#  $version = '9.7.0.27'
-  $version = '9.7.1.16'
+  $version = '9.7.0.27'
+#  $version = '9.7.1.16'
 
 
-  class { "caapm::database":
-    version => "$version",
-    user_install_dir => 'C:/Ecetera/Introscope/',
-    database => 'postgres',
-    postgres_dir => 'C:/Ecetera/PostgreSQL/',
-  } 
+#  class { "caapm::database":
+  caapm::database { 'apmdb':
+    version          => "${version}",
+    user_install_dir => "C:/Ecetera/Introscope${version}/",
+    database         => 'postgres',
+    postgres_dir     => 'C:/Ecetera/PostgreSQL/',
+    owner            => 'Administrator',
+    group            => 'Administrators',
+  }
 }
