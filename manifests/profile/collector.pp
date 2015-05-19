1b15aae3eb153b9ebddb40810a1444cd0f33ecf2
dimarra <raul.dimar@gmail.com>
2015-05-19 16:44:06 +1000

    puppet manage domains.xml

--- manifests/profile/collector.pp
+++ manifests/profile/collector.pp
@@ -15,7 +15,6 @@ class caapm::profile::collector {
   $group                       = 'root'
   $em_java_opts_addtl          = '-Xms512m -Xmx512m -XX:MaxPermSize=256m -Dorg.owasp.esapi.resources=./config/esapi'
   $puppet_src                  = "puppet:///modules/${module_name}"
-  $apm_help_url                = "http://${::fqdn}:8081/IntroscopeHelp/Monitoring-System-Performance-and-Problems_80348605.html"
 
 
   caapm::em {'collector':
@@ -29,45 +28,13 @@ class caapm::profile::collector {
     db_host                     => $db_host,
     em_service_name             => $em_service_name,
     config_em_as_service        => $config_em_as_service,
-    start_em_as_service         => false,
     owner                       => $owner,
     group                       => $group,
-  }
-
-  # establish dependency order
-  ->
-  class { 'caapm::config::em_lax':
-    version            => $version,
-    em_home            => $em_home,
-    em_java_opts_addtl => $em_java_opts_addtl,
-    owner              => $owner,
-    group              => $group,
-  }
-  ->
-  class { 'caapm::config::em_properties':
-    version                     => $version,
-    em_home                     => $em_home,
-    cluster_role                => $cluster_role,
-    transactionevents_dir       => '/var/caapm/traces',
-    smartstor_dir               => '/var/caapm/smartstor',
-    threaddump_dir              => '/var/caapm/threaddumps',
-    smartstor_archive           => '/var/caapm/smartstor/archive',
-    baselines_dir               => '/var/caapm/smartstor',
-#    snmp_enable                 => true,
-#    scauth_enable               => true,
-#    catalyst_snmp_enable        => true,
-#    catalyst_snmp_destination_host => 'redhat1',
-    soa_deviation_enabled       => true,
-    apm_help_url                => $apm_help_url,
-    owner                       => $owner,
-    group                       => $group,
-  }
-  ->
-  class { 'caapm::config::apm_events_thresholds':
-    version => $version,
-    em_home => $em_home,
-    owner   => $owner,
-    group   => $group,
+    notify                      => [ Class['caapm::config::em_lax'],
+                                     Class['caapm::config::em_properties'],
+                                     Class['caapm::config::apm_events_thresholds'],
+                                     Class['caapm::config::domains_xml'],
+                                     File['IntroscopeHelp.war']]
   }
   ->
   tidy { ["$em_home/config/modules"]:
@@ -80,14 +47,55 @@ class caapm::profile::collector {
     owner   => $owner,
     group   => $group,
   }
-  ->
+
+
+
+  class { 'caapm::config::em_lax':
+    version            => $version,
+    em_home            => $em_home,
+    em_java_opts_addtl => $em_java_opts_addtl,
+    owner              => $owner,
+    group              => $group,
+  }
+
+  class { 'caapm::config::em_properties':
+    version                     => $version,
+    em_home                     => $em_home,
+    cluster_role                => $cluster_role,
+    transactionevents_dir       => '/var/caapm/traces',
+    smartstor_dir               => '/var/caapm/smartstor',
+    threaddump_dir              => '/var/caapm/threaddumps',
+    smartstor_archive           => '/var/caapm/smartstor/archive',
+    baselines_dir               => '/var/caapm/smartstor',
+#    snmp_enable                 => true,
+#    scauth_enable               => true,
+#    catalyst_snmp_enable        => true,
+#    soa_deviation_enabled       => true,
+    owner                       => $owner,
+    group                       => $group,
+  }
+
+  class { 'caapm::config::apm_events_thresholds':
+    version => $version,
+    em_home => $em_home,
+    owner   => $owner,
+    group   => $group,
+  }
+
   file { 'IntroscopeHelp.war':
-    ensure => present,
-    force  => true,
-    path   => "${em_home}/webapps/IntroscopeHelp.war",
-    source => "${puppet_src}/${version}/IntroscopeHelp.war",
-    owner  => $owner,
-    group  => $group,
+    ensure  => present,
+    force   => true,
+    path    => "${em_home}/webapps/IntroscopeHelp.war",
+    source  => "${puppet_src}/${version}/IntroscopeHelp.war",
+    owner   => $owner,
+    group   => $group,
+  }
+
+  class { 'caapm::config::domains_xml':
+    version => $version,
+    em_home => $em_home,
+    owner   => $owner,
+    group   => $group,
   }
 
 
--- manifests/profile/manager.pp
+++ manifests/profile/manager.pp
@@ -1,16 +1,24 @@
-# 
+#
 # == Class: caapm::profile::manager
 #
-# This profile configures the defaults for a Manager of Managers (MOM) Enterprise Manager 
+# This profile configures the defaults for a Manager of Managers (MOM) Enterprise Manager
 #
 
-class caapm::profile::manager { 
+class caapm::profile::manager {
+
+  $version                     = '9.7.1.16'
+  $em_home                     = "/opt/caapm/Introscope${version}/"
+  $owner                       = 'root'
+  $group                       = 'root'
+  $cluster_role                = 'Manager'
+
+
   caapm::em {'mom':
-    version                     => '9.6.0.0',
-    user_install_dir            => "/opt/caapm/Introscope9.6.0.0/",
+    version                     => $version,
+    user_install_dir            => $em_home,
     features                    => 'Enterprise Manager',
     clusterEM                   => true,
-    cluster_role                => 'Manager',
+    cluster_role                => $cluster_role,
     txnTraceDir                 => '/var/caapm/traces',
     smartstor_dir               => '/var/caapm/smartstor',
     threaddump_dir              => '/var/caapm/threaddumps',
@@ -19,7 +27,16 @@ class caapm::profile::manager {
     db_host                     => 'win28r2.diamond.org',
     em_service_name             => 'introscope',
     config_em_as_service        => true,
-  } 
+    wv_service_name             => 'webview',
+    config_wv_as_service        => true,
+  }
 
-  
+  class { 'caapm::config::wv_lax':
+    version            => $version,
+    em_home            => $em_home,
+    owner              => $owner,
+    group              => $group,
+  }
+
+
 }
