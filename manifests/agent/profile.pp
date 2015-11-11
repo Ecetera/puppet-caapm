6c82ae397a70befbca0672ae7ce52271495983fc
chuahm <mingxen.chuah@anz.com>
2015-11-11 14:15:50 +1100

    Made Profile a class.

--- manifests/agent/profile.pp
+++ manifests/agent/profile.pp
@@ -1,7 +1,10 @@
-define profile (
-  $version = "${caapm::agent::version}",
-  $agents_dir = "${caapm::agent::agents_dir}",
-){
+
+class caapm::agent::profile inherits caapm::agent {
+
+  if $caller_module_name != $module_name {
+    fail("Use of private class ${name} by ${caller_module_name}")
+  }
+
   if $version == undef {
           fail('Valid version not provided')
   }
@@ -13,7 +16,7 @@ define profile (
     path   => "${agents_dir}/wily/core/config/IntroscopeAgent-${name}.ppmanaged",
     force  => true,
     content => template("${module_name}/${version}/agent/IntroscopeAgent-${name}.profile"),
-    owner  => $owner,
+    owner  => $::owner,
     group  => $group,
     mode   => $mode,
     notify =>  Exec["updating ${name} profile"],
@@ -24,4 +27,38 @@ define profile (
     command     => "/bin/cp -p IntroscopeAgent-${name}.ppmanaged IntroscopeAgent-${name}.profile",
     user        => $owner,
   }
+
 }
+
+
+
+
+
+
+#define profile (
+#  $version = "${caapm::agent::version}",
+#  $agents_dir = "${caapm::agent::agents_dir}",
+#){
+#  if $version == undef {
+#          fail('Valid version not provided')
+#  }
+#
+#  notify {"Running agent::config with ${name} profile":}
+#
+#  file { "applying ${name} profile":
+#    ensure => present,
+#    path   => "${agents_dir}/wily/core/config/IntroscopeAgent-${name}.ppmanaged",
+#    force  => true,
+#    content => template("${module_name}/${version}/agent/IntroscopeAgent-${name}.profile"),
+#    owner  => $::owner,
+#    group  => $group,
+#    mode   => $mode,
+#    notify =>  Exec["updating ${name} profile"],
+#  }
+#
+#  exec { "updating ${name} profile":
+#    cwd         => "${agents_dir}/wily/core/config",
+#    command     => "/bin/cp -p IntroscopeAgent-${name}.ppmanaged IntroscopeAgent-${name}.profile",
+#    user        => $owner,
+#  }
+#}
